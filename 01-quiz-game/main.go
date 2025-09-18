ae3acf34fe016d313983a4fe7a452560bd6bcccd
fsunesara <faisalswave@gmail.com>
2025-09-18 10:35:11 -0500

    exercise 1 part 2

--- 01-quiz-game/main.go
+++ 01-quiz-game/main.go
@@ -1,8 +1,78 @@
 package main
 
-import "flag"
+import (
+	"encoding/csv"
+	"flag"
+	"fmt"
+	"os"
+	"strings"
+	"time"
+)
+
+func readCsv(fileName string) [][]string {
+	f, err := os.ReadFile(fileName)
+	if err != nil {
+		panic(err)
+	}
+	reader := csv.NewReader(strings.NewReader(string(f)))
+
+	questions, err := reader.ReadAll()
+	if err != nil {
+		panic(err)
+	}
+
+	return questions
+}
+
+func quiz1(fileName string) {
+	questions := readCsv(fileName)
+	score := 0
+	numQuestions := len(questions)
+	for _, question := range questions {
+		var answer string
+		fmt.Printf("%s = ", question[0])
+		fmt.Scanln(&answer)
+		if answer == question[1] {
+			score++
+		}
+	}
+
+	endQuiz(score, numQuestions)
+}
+
+func quiz2(fileName string, timeLimit int) {
+	timer := time.NewTimer(time.Duration(timeLimit) * time.Second)
+	questions := readCsv(fileName)
+	score := 0
+	numQuestions := len(questions)
+
+	go func() {
+		<-timer.C
+		fmt.Println("\ntimeout")
+		endQuiz(score, numQuestions)
+	}()
+
+	for _, question := range questions {
+		var answer string
+		fmt.Printf("%s = ", question[0])
+		fmt.Scanln(&answer)
+		if answer == question[1] {
+			score++
+		}
+	}
+
+	endQuiz(score, numQuestions)
+}
+
+func endQuiz(score int, numQuestions int) {
+	fmt.Printf("You scored %d out of %d.\n", score, numQuestions)
+	os.Exit(0)
+}
 
 func main() {
 	fileName := flag.String("file", "problems.csv", "a csv file in the format of 'question,answer'")
-	Quiz1(*fileName)
+	timeLimit := flag.Int("timeLimit", 30, "the time limit for the quiz in seconds")
+	flag.Parse()
+	// quiz1(*fileName)
+	quiz2(*fileName, *timeLimit)
 }
